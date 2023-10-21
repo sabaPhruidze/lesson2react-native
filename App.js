99461e29d194bdf84ebbbc396640254c7bd56e07
Saba Phruidze <sabaphruidze01@gmail.com>
2023-10-21 13:13:51 +0400

    I did the same but now it was {} I understand that logic part works as it worked in the react.js

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("shaun");
+  const [person, setPerson] = useState({ name: "mario", age: 40 });
   const clickHandler = () => {
     setName("chung-lee");
+    setPerson({ name: "luigi", age: 45 });
   };
   return (
     <View style={styles.container}>
       <Text>my name is {name}</Text>
-      <Text></Text>
+      <Text>
+        his name is {person.name} with the age {person.age}
+      </Text>
       <View style={styles.buttonContainer}>
         <Button title="update state" onPress={clickHandler} />
       </View>
